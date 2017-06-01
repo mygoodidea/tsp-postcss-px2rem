1f0146e0a9494e3a9fd1677364c67fb39202dcb3
tianshupei <tianshupei88@163.com>
2017-06-01 18:04:08 +0800

    fix

--- test/index-test.js
+++ test/index-test.js
@@ -1,10 +1,31 @@
 /**
  * Created by tianshupei on 2017/6/1.
  */
+var assert = require('assert')
+
 var postcss = require('postcss')
 var px2rem = require('../lib')
 
-var css = 'h1 { font-size: 20px; /* rem */}'
+var rem = 'h1 { font-size: 20px; /* rem  */}'
+var px = 'h1 { font-size: 20px; /* px */}'
 
-console.log(postcss(px2rem).process(css).css)
+const processor = postcss([px2rem])
 
+
+describe('#lib/index.js', function () {
+    it('shoud replace px to rem', function () {
+        var result = processor.process(rem).css
+        assert.strictEqual(
+            result.indexOf('px') === -1 && result.indexOf('rem') !== -1,
+            true
+        )
+    })
+
+    it('no replace', function () {
+        var result = processor.process(px).css
+        assert.strictEqual(
+            result.indexOf('px') !== -1,
+            true
+        )
+    })
+})
